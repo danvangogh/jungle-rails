c197d6cfafa223f18bbebb675320e07f6463b5d0
danvangogh <djameswhite@yahoo.com>
2018-12-07 04:52:30 +0000

    email sending line items and subtotal

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
     if order.valid?
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
-      ApplicationMailer.application_mailer(current_user.email).deliver_now
+      ApplicationMailer.application_mailer(current_user.email, order).deliver_now
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,9 +2,10 @@ class ApplicationMailer < ActionMailer::Base
   default from: "no-reply@jungle.com"
   layout 'mailer'
 
-  def application_mailer(email)
-    puts "GOOOOOT HEEREEEEEE!!!!!!!!!"
-    mail(to: @email, subject: 'Order Confirmed!',
+  def application_mailer(email, order)
+    @order = order
+    @email = email
+    mail(to: @email, subject: "Order #{@order.id} Confirmed",
     template_path: '../views/layouts',
     template_name: 'application_mailer')
   end
