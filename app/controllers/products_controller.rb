e18d1e922cb921c99ab4cb9271a8f0e546bca979
danvangogh <djameswhite@yahoo.com>
2019-01-19 01:25:00 +0000

    git test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    puts "hello world"
+    render json: @products
+    # render('views/products/index.html.erb', templateVars = {@products})
   end
 
   def show
